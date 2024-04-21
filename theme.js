2e6e144c7f93cf2fd44856bd2a8f02ef66f4c831
Kurogee <taiti.nakaya.2009@gmail.com>
2024-04-21 15:33:58 +0900

    バッジの追加、CSSの追加・変更、などなど

---
+++ theme.js
@@ -0,0 +1,11 @@
+const theme_css = {
+    "default_theme" : {
+        "path" : "./theme_css/default_theme.css",
+        "need_level" : 0
+    },
+
+    "gaming_theme" : {
+        "path" : "./theme_css/black_theme.css",
+        "need_level" : 5
+    },
+}
