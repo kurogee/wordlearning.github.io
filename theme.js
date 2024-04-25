26cf3baf280eebe5dca6d1bb5309cd65efcad408
Kurogee <taiti.nakaya.2009@gmail.com>
2024-04-25 22:56:42 +0900

    QaAJのバグを修正、talkにパート追加、CSSの微量な変更など

--- theme.js
+++ theme.js
@@ -1,11 +1,23 @@
 const theme_css = {
     "default_theme" : {
-        "path" : "./theme_css/default_theme.css",
+        "path" : "default_theme.css",
         "need_level" : 0
     },
 
     "gaming_theme" : {
-        "path" : "./theme_css/black_theme.css",
+        "path" : "black_theme.css",
         "need_level" : 5
     },
+}
+
+function change_theme(parent_path, theme_name) {
+    let theme = theme_css[theme_name];
+    let theme_path = parent_path + theme["path"];
+    let theme_need_level = theme["need_level"];
+    let user_level = get_user_level();
+
+    if (user_level >= theme_need_level) {
+        let link = $("head link[rel='stylesheet']")[0];
+        link.href = theme_path;
+    }
 }
